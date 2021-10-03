6bdf95ea19795daa58cc9fa5edb8d72631ecec8e
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2021-10-02 19:22:53 -0500

    added the generatePassword function and capturing password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
-
+function generatePassword() {
+  // Capture users password length
+  var pLength = Number(window.prompt("Enter a length of at least 8 characters and no more than 128 characters."));
+  console.log(pLength);
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -8,9 +12,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
